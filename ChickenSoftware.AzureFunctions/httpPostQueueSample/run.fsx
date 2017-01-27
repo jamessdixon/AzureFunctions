2625c171e18c151900832d9668afe611e1f392f6
Jamie Dixon <jamessdixon@gmail.com>
2017-01-27 07:54:13 -0500

    initial commit

---
+++ ChickenSoftware.AzureFunctions/httpPostQueueSample/run.fsx
@@ -0,0 +1,11 @@
+﻿open System
+
+let Run(personItem: string, 
+        expirationTime: DateTimeOffset, 
+        insertionTime: DateTimeOffset , 
+        nextVisibleTime: DateTimeOffset ,
+        queueTrigger: string ,
+        id: string ,
+        popReceipt: string ,
+        dequeueCount: int,
+        log: TraceWriter) = log.Info("Hello")
